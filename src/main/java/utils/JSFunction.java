5ed884a0eb345a0f04938784340b213f45f5794e
21-manic-010g <21-manic-010g@21-manic-010>
2024-06-10 14:08:45 +0900

    123

--- src/main/java/utils/JSFunction.java
+++ src/main/java/utils/JSFunction.java
@@ -1,5 +1,8 @@
 package utils;
 
+import java.io.PrintWriter;
+
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.jsp.JspWriter;
 
 public class JSFunction {
@@ -14,6 +17,19 @@ public class JSFunction {
 		} catch (Exception e) {
 		}
 	}
+	
+	public static void alertLocation(HttpServletResponse resp, String msg, String url) {
+		try {
+			resp.setContentType("text/html;charset=UTF-8");
+			PrintWriter writer = resp.getWriter();
+			String script = ""
+					+ "<script>"
+					+ "	alert('" + msg + "');"
+					+ "</script>";
+			writer.print(script);
+		} catch (Exception e) {
+		}
+	}
 
 	public static void alertBack(String msg, JspWriter out) {
 		try {
@@ -23,6 +39,20 @@ public class JSFunction {
 					+ "	history.back();"
 					+ "</script>";
 			out.println(script);
+		} catch (Exception e) {
+		}
+	}
+	
+	public static void alertBack(HttpServletResponse resp, String msg) {
+		try {
+			resp.setContentType("text/html;charset=UTF-8");
+			PrintWriter writer = resp.getWriter();
+			String script = ""
+					+ "<script>"
+					+ "	alert('" + msg + "');"
+					+ "	history.back();"
+					+ "</script>";
+			writer.println(script);
 		} catch (Exception e) {
 		}
 	}
